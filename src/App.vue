9db08fd29e06a7d2eb595c44e94b138c38aa3bad
woodtick88 <woodtick88@gmail.com>
2018-05-26 12:05:02 +0300

    basic markup toolbar
    basic markup toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,63 @@
 <template>
-  <h1>Vue Test {{ title }}</h1>
+    <v-app>
+      <v-navigation-drawer app temporary>
+
+          <v-toolbar flat>
+              <v-list>
+                <v-list-tile>
+                  <v-list-tile-title class="title">
+                    Application
+                  </v-list-tile-title>
+                </v-list-tile>
+              </v-list>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list dense class="pt-0">
+              <v-list-tile v-for="item in items" :key="item.title" @click="">
+                <v-list-tile-action>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+
+      </v-navigation-drawer>
+
+      <v-toolbar app dark color="primary">
+
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>Title</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-btn flat>Link One</v-btn>
+            <v-btn flat>Link Two</v-btn>
+            <v-btn flat>Link Three</v-btn>
+          </v-toolbar-items>
+
+      </v-toolbar>
+
+      <v-content>
+        <v-container fluid>
+          <router-view></router-view>
+        </v-container>
+      </v-content>
+      <v-footer app></v-footer>
+    </v-app>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-      title: "Hello"
+      title: "Hello2!!",
+      items: [
+          { title: 'Home', icon: 'dashboard' },
+          { title: 'About', icon: 'question_answer' }
+        ],
+      right: null
     }
-  },
-  name: 'App'
+  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   router,

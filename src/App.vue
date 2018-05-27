b3c53c64641a1b47f61675215279f9989011a881
woodtick88 <woodtick88@gmail.com>
2018-05-27 21:12:26 +0300

    update app

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,7 @@
   <v-navigation-drawer app temporary v-model="drawer">
 
     <v-list>
-      <v-list-tile
-        v-for="link in links"
-        :key="link.title"
-        :to="link.url"
-      >
+      <v-list-tile v-for="link in links" :key="link.title" :to="link.url">
         <v-list-tile-action>
           <v-icon>{{ link.icon }}</v-icon>
         </v-list-tile-action>
@@ -17,23 +13,16 @@
       </v-list-tile>
     </v-list>
 
+
   </v-navigation-drawer>
 
   <v-toolbar app dark color="primary">
 
-    <v-toolbar-side-icon
-        @click="drawer = !drawer"
-        class="hidden-md-and-up"
-    ></v-toolbar-side-icon>
+    <v-toolbar-side-icon @click="drawer = !drawer" class="hidden-md-and-up"></v-toolbar-side-icon>
     <v-toolbar-title>Ad application</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn
-        v-for="link in links"
-        :key="link.title"
-        :to="link.url"
-        flat
-      >
+      <v-btn v-for="link in links" :key="link.title" :to="link.url" flat>
         <v-icon left>{{ link.icon }}</v-icon>
         {{ link.title }}
       </v-btn>
@@ -54,12 +43,31 @@
 export default {
   data() {
     return {
-      links: [
-          {title: 'Login', icon: 'lock', url: '/login'},
-          {title: 'Registration', icon: 'face', url: '/registration'},
-          {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
-          {title: 'New ad', icon: 'note_add', url: '/new'},
-          {title: 'My ads', icon: 'list', url: '/list'}
+      links: [{
+          title: 'Login',
+          icon: 'lock',
+          url: '/login'
+        },
+        {
+          title: 'Registration',
+          icon: 'face',
+          url: '/registration'
+        },
+        {
+          title: 'Orders',
+          icon: 'bookmark_border',
+          url: '/orders'
+        },
+        {
+          title: 'New ad',
+          icon: 'note_add',
+          url: '/new'
+        },
+        {
+          title: 'My ads',
+          icon: 'list',
+          url: '/list'
+        }
       ],
       drawer: false
     }

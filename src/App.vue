15beb174292d260151a3e2eeb49b9df7e23f9472
woodtick88 <woodtick88@gmail.com>
2018-05-27 12:51:31 +0300

    update readme

--- src/App.vue
+++ src/App.vue
@@ -31,9 +31,10 @@
           <v-toolbar-title>Title</v-toolbar-title>
           <v-spacer></v-spacer>
           <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat>Link One</v-btn>
-            <v-btn flat>Link Two</v-btn>
-            <v-btn flat>Link Three</v-btn>
+            <v-btn flat>
+                <v-icon left>bug_report</v-icon>
+                Link One
+            </v-btn>
           </v-toolbar-items>
 
       </v-toolbar>

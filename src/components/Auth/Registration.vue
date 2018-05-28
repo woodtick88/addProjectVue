4cb8c55f6434a2bcd3be0a60fade165c6c030d83
woodtick88 <woodtick88@gmail.com>
2018-05-28 15:50:36 +0300

    add Registration page

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,21 +1,56 @@
 <template>
-
-    <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Registration</h1>
-            </v-flex>
-        </v-layout>
-    </v-container>
-
+<v-container fluid fill-height>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+      <v-card class="elevation-12">
+        <v-toolbar dark color="primary">
+          <v-toolbar-title>Registration form</v-toolbar-title>
+        </v-toolbar>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" validation>
+            <v-text-field prepend-icon="person" name="email" label="Email" type="email" v-model="email" :rules="emailRules"></v-text-field>
+            <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password" v-model="password" :counter="6" :rules="passwordRules"></v-text-field>
+            <v-text-field id="confirm-password" prepend-icon="lock" name="confirm-password" label="Confirm password" type="password" v-model="confirmPassword" :rules="confirmPasswordRules"></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" @click="onSubmit" :disabled="!valid">Create account</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</v-container>
 </template>
 
 <script>
-
 export default {
-    data() {
-        return {}
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => v.length >= 6 || 'Password must be more than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Confirm password is required',
+        v => v === this.password || 'Password does not match'
+      ]
     }
+  },
+  methods: {
+    onSubmit() {
+      if (this.$refs.form.validate()) {
+        console.log(this.email + ' ' + this.password + ' ' + this.confirmPassword)
+      }
+    }
+  }
 }
-
 </script>

42f6fae4d6a98aa329160f97a057e1741e2f98cc
woodtick88 <woodtick88@gmail.com>
2018-05-30 12:47:28 +0300

    add Ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -3,7 +3,22 @@
     <v-container>
         <v-layout row>
             <v-flex xs12>
-                <h1>Ad</h1>
+                <v-card>
+                    <v-card-media
+                      height="300px"
+                      src="https://vuetifyjs.com/static/doc-images/carousel/sky.jpg"
+                    >
+                    </v-card-media>
+                    <v-card-text>
+                        <h1 class="text--primary">Test</h1>
+                        <p>test text, test text</p>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn class="warning">Edit</v-btn>
+                        <v-btn class="success">Buy</v-btn>
+                    </v-card-actions>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>

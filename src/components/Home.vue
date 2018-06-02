de92ceb4267fea91b08424f80ac877f7b6cb2cb9
woodtick88 <woodtick88@gmail.com>
2018-06-02 19:39:31 +0300

    add progress loading for home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,57 @@
 <template>
-<div>
-  <v-container fluid>
-    <v-layout row>
-      <v-flex xs12>
-        <v-carousel delimiter-icon="stop">
-          <v-carousel-item v-for="ad in promoAds" :src="ad.imageSrc" :key="ad.id">
-            <div class="car-link">
-              <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
-            </div>
-          </v-carousel-item>
-        </v-carousel>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    <div>
+        <div v-if ="!loading">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs12>
+                <v-carousel delimiter-icon="stop">
+                  <v-carousel-item v-for="ad in promoAds" :src="ad.imageSrc" :key="ad.id">
+                    <div class="car-link">
+                      <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+                    </div>
+                  </v-carousel-item>
+                </v-carousel>
+              </v-flex>
+            </v-layout>
+          </v-container>
 
-  <v-container grid-list-lg>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 md6 lg4 v-for="ad in ads" :key="ad.id">
-        <v-card>
-          <v-card-media :src="ad.imageSrc" height="200px">
-          </v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">{{ ad.title }}</h3>
-              <div>{{ ad.description }}</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
-            <v-btn flat class="primary">Buy</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+          <v-container grid-list-lg>
+            <v-layout row wrap>
+              <v-flex xs12 sm6 md6 lg4 v-for="ad in ads" :key="ad.id">
+                <v-card>
+                  <v-card-media :src="ad.imageSrc" height="200px">
+                  </v-card-media>
+                  <v-card-title primary-title>
+                    <div>
+                      <h3 class="headline mb-0">{{ ad.title }}</h3>
+                      <div>{{ ad.description }}</div>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+                    <v-btn flat class="primary">Buy</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
 
-</div>
+        </div>
+        <div else >
+            <v-container>
+                <v-layout row>
+                    <v-flex xs12 class="text-xs-center pt-5">
+                        <v-progress-circular
+                            :size="120"
+                            indeterminate
+                            color="primary"
+                        ></v-progress-circular>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -47,20 +62,27 @@ export default {
     },
     ads() {
       return this.$store.getters.ads
-    }
+  },
+  loading() {
+      return this.$store.getters.loading
+  }
+
   }
 }
 </script>
 
 <style scoped>
-.car-link {
-  position: absolute;
-  bottom: 50px;
-  left: 50%;
-  background: rgba(0, 0, 0, 0.5);
-  transform: translate(-50%, 0);
-  padding: 5px 15px;
-  border-top-right-radius: 5px;
-  border-top-left-radius: 5px;
-}
+
+    .car-link {
+      position: absolute;
+      bottom: 50px;
+      left: 50%;
+      background: rgba(0, 0, 0, 0.5);
+      transform: translate(-50%, 0);
+      padding: 5px 15px;
+      border-top-right-radius: 5px;
+      border-top-left-radius: 5px;
+    }
+
+
 </style>

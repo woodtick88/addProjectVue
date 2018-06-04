c99fa35080c9717e06ea0315d5d0677d35b9eb69
woodtick88 <woodtick88@gmail.com>
2018-06-04 23:42:57 +0300

    add order processing

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,14 +1,27 @@
 <template>
     <v-container>
       <v-layout row>
-        <v-flex xs12 sm6 offset-sm3>
+
+        <v-flex xs12 class="text-xs-center pt-5"
+          v-if="loading"
+        >
+          <v-progress-circular
+            :size="120"
+            indeterminate
+            color="primary"
+          ></v-progress-circular>
+        </v-flex>
+
+        <v-flex xs12 sm6 offset-sm3
+          v-else-if="!loading && orders.length !== 0"
+        >
           <v-list two-line subheader>
             <h1 class="text--secondary mb-3 ml-3 pt-3">Orders</h1>
             <v-list-tile avatar v-for="order in orders" :key="order.id">
               <v-list-tile-action>
                 <v-checkbox
                   color="success"
-                  @change="markDone(order)"                   
+                  @change="markDone(order)"
                   :input-value="order.done"
                 ></v-checkbox>
               </v-list-tile-action>
@@ -22,27 +35,41 @@
             </v-list-tile>
           </v-list>
         </v-flex>
+
+        <v-flex xs12 class="text-xs-center pt-5"
+          v-else
+        >
+          <h1 class="text--secondary">You have no orders</h1>
+        </v-flex>
+
       </v-layout>
     </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      orders: [{
-        id: 'order-1',
-        name: "item-10",
-        phone: '7-888-888-88-88',
-        adId: '10',
-        done: false
-      }]
+
+  computed: {
+    loading() {
+      return this.$store.getters.loading
+    },
+    orders() {
+      return this.$store.getters.orders
     }
+
   },
   methods: {
     markDone(order) {
-      order.done = !order.done
+      this.$store.dispatch('markOrderDone', order.id)
+        .then(() => {
+          order.done = true
+        })
+        .catch(() => {})
+
     }
+  },
+  created() {
+    this.$store.dispatch('fetchOrders')
   }
 
 }
--- src/store/orders.js
+++ src/store/orders.js
@@ -1,20 +1,88 @@
+import * as firebase from 'firebase'
+
+class Order {
+  constructor(name, phone, adId, done = false, id = null) {
+    this.name = name
+    this.phone = phone
+    this.adId = adId
+    this.done = done
+    this.id = id
+  }
+}
+
 export default {
   state: {
     orders: []
   },
   mutations: {
-    createOrder(state, payload) {
-      state.orders.push(payload)
+    loadOrders(state, payload) {
+      state.orders = payload
     }
   },
   actions: {
-    async createOrder() {
-      await new Promise((resolve, reject) => {
-        setTimeout(() => {
-          resolve()
-        }, 4000)
-      })
+    async createOrder({commit, getters}, {name, phone, adId, ownerId}) {
+      let order = new Order(name, phone, adId)
+      commit('clearError')
+
+      try {
+        await firebase.database().ref(`/users/${getters.user.id}/orders`).push(order)
+      } catch(error) {
+        commit('setError', error.message)
+        throw error
+      }
+
+    },
+    async fetchOrders({commit, getters}) {
+      commit('setLoading', true)
+      commit('clearError')
+
+      let resultOrders = []
+
+      try {
+        let fbVal = await firebase.database().ref(`/users/${getters.user.id}/orders`).once('value')
+        let orders = fbVal.val()
+
+        Object.keys(orders).forEach(key => {
+          let order = orders[key]
+          resultOrders.push(
+            new Order(order.name, order.phone, order.adId, order.done, key)
+          )
+        })
+
+        commit('loadOrders', resultOrders)
+        commit('setLoading', false)
+      } catch(error) {
+        commit('setLoading', false)
+        commit('setError', error.message)
+        throw error
+      }
+    },
+    async markOrderDone({commit, getters}, payload) {
+      commit('clearError')
+        await firebase.database().ref(`/users/${getters.user.id}/orders`).child(payload).update({
+          done: true
+        })
+      try {
+
+      } catch(error) {
+        commit('setError', error.message)
+        throw error
+      }
+
     }
+
+
+
   },
-  getters: {}
+  getters: {
+    doneOrders(state) {
+      return state.orders.filter(order => order.done)
+    },
+    undoneOrders(state) {
+      return state.orders.filter(order => !order.done)
+    },
+    orders(state, getters) {
+      return getters.undoneOrders.concat(getters.doneOrders)
+    }
+  }
 }
